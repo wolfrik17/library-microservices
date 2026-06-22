daaf3700e3cf1c8101c22619cd87ac0d6be88573
wolfrik17 <github@aioanei.com>
2026-06-22 21:39:46 +0300

    generated microservices using start.spring.io

---
+++ lending-service/src/main/java/com/library/lending_service/LendingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.library.lending_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingServiceApplication.class, args);
+	}
+
+}
---
+++ lending-service/src/test/java/com/library/lending_service/LendingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.lending_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
